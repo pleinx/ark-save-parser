0be424c3c8c7c58fb0b9c9902bcd04b8f0ad4a1c
O-S Marin <K07H@users.noreply.github.com>
2025-09-09 16:57:03 +0200

    Removed useless game_bin and game_obj params
    * Removed useless params "game_bin: Optional[ArkBinaryParser] = None" and "game_obj: Optional[ArkGameObject] = None".
    * Added "from_object()" static method in dino_ai_controller.py.

--- src/arkparse/object_model/structures/structure.py
+++ src/arkparse/object_model/structures/structure.py
@@ -51,8 +51,8 @@ class Structure(ParsedObjectBase):
     #MyInventoryComponent
     #NetDestructionTime
 
-    def __init__(self, uuid: UUID, save: AsaSave, game_bin: Optional[ArkBinaryParser] = None, game_obj: Optional[ArkGameObject] = None):
-        super().__init__(uuid, save=save, game_bin=game_bin, game_obj=game_obj)
+    def __init__(self, uuid: UUID, save: AsaSave):
+        super().__init__(uuid, save=save)
 
         properties = self.object
         self.owner = ObjectOwner(properties)
