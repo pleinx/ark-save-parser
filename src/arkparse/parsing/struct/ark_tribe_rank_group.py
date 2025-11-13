763a005fcae5d51fb66a9c8c67cfa7351edf9980
Vincent Henau <vincent.henau@scioteq.com>
2025-11-13 14:57:38 +0100

    Fix tribe rank group parsing

--- src/arkparse/parsing/struct/ark_tribe_rank_group.py
+++ src/arkparse/parsing/struct/ark_tribe_rank_group.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
+import json
+
+from arkparse.utils.json_utils import DefaultJsonEncoder
 
 if TYPE_CHECKING:
     from ..ark_binary_parser import ArkBinaryParser
@@ -42,19 +45,29 @@ class ArkTribeRankGroup:
     def _read_byte_property(self, byte_buffer: "ArkBinaryParser", name: str) -> int:
         byte_buffer.validate_string(name)
         byte_buffer.validate_string("ByteProperty")
-        byte_buffer.skip_bytes(4)
-        is_pos = byte_buffer.read_byte() == 1
-        if is_pos:
-            pos = byte_buffer.read_uint32()
-        value = byte_buffer.read_byte()
-        return value
+        present = byte_buffer.read_uint32() != 1
+
+        if present:
+            is_pos = byte_buffer.read_byte() == 1
+            if is_pos:
+                pos = byte_buffer.read_uint32()
+            value = byte_buffer.read_byte()
+            return value
+        else:
+            byte_buffer.validate_uint32(0)
+            byte_buffer.validate_string("None")
+            byte_buffer.validate_uint16(0)
     
     def _read_bool_property(self, byte_buffer: "ArkBinaryParser", name: str) -> bool:
         byte_buffer.validate_string(name)
         byte_buffer.validate_string("BoolProperty")
         byte_buffer.validate_uint32(0)
         byte_buffer.validate_uint32(0)
-        value = byte_buffer.read_byte()
+        value = byte_buffer.peek_u16()
+        if value == 1 or value == 0:
+            byte_buffer.read_uint16()
+        else:
+            value = byte_buffer.read_byte()
         return value != 0
 
     def __init__(self, byte_buffer: "ArkBinaryParser"):
@@ -102,9 +115,44 @@ class ArkTribeRankGroup:
 
         byte_buffer.validate_string("None")
 
-        print(f"Parsed TribeRankGroup: {self}")
+        print(self.to_json_str())
 
     def __str__(self) -> str:
         return (f"name:{self.name} rank:{self.rank} default:{self.is_default_rank}")
+    
+    def to_json_obj(self):
+        return {
+            "name": self.name,
+            "rank": self.rank,
+            "inventory_rank": self.inventory_rank,
+            "structure_activation_rank": self.structure_activation_rank,
+            "new_structure_activation_rank": self.new_structure_activation_rank,
+            "new_structure_inventory_rank": self.new_structure_inventory_rank,
+            "pet_order_rank": self.pet_order_rank,
+            "pet_riding_rank": self.pet_riding_rank,
+            "invite_to_group_rank": self.invite_to_group_rank,
+            "max_promotion_group_rank": self.max_promotion_group_rank,
+            "max_demotion_group_rank": self.max_demotion_group_rank,
+            "max_banishment_group_rank": self.max_banishment_group_rank,
+            "num_invites_remaining": self.num_invites_remaining,
+            "prevent_structure_demolish": self.prevent_structure_demolish,
+            "prevent_structure_attachment": self.prevent_structure_attachment,
+            "prevent_structure_build_in_range": self.prevent_structure_build_in_range,
+            "prevent_unclaiming": self.prevent_unclaiming,
+            "allow_invites": self.allow_invites,
+            "limit_invites": self.limit_invites,
+            "allow_demotions": self.allow_demotions,
+            "allow_promotions": self.allow_promotions,
+            "allow_banishments": self.allow_banishments,
+            "prevent_wireless_crafting": self.prevent_wireless_crafting,
+            "teleport_members_rank": self.teleport_members_rank,
+            "teleport_dinos_rank": self.teleport_dinos_rank,
+            "is_default_rank": self.is_default_rank,
+            "allow_ping": self.allow_ping,
+            "allow_rally_point": self.allow_rally_point
+        }
+    
+    def to_json_str(self):
+        return json.dumps(self.to_json_obj(), default=lambda o: o.to_json_obj() if hasattr(o, 'to_json_obj') else None, indent=4, cls=DefaultJsonEncoder)
 
 
